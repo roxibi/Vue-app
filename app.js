67a1843991e24be5b1f5b41fc21d4ccd4fcd1322
Roxana Bita <roxana.bita@gmail.com>
2023-03-16 12:34:30 +0100

    simple favorite books filter

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const app = Vue.createApp({
     //template: '<h2>I am the template</h2>'
     data() {
         return {
+            url:'https://www.google.com',
             showBook: true,
-            title: "Ansichten eines Clowns",
-            author: "Heinricht Böll",
-            age: 45,
-            place: "Bonn",
+            books: [
+                {       title: "1Ansichten eines Clowns",
+                author: "1Heinricht Böll", img:'http://placekitten.com/200/300', isFav:true,},
+                {       title: "2Ansichten eines Clowns",
+                author: "2Heinricht Böll", img:'http://placekitten.com/200/300', isFav:false,},
+                {       title: "3Ansichten eines Clowns",
+                author: "3Heinricht Böll", img:'http://placekitten.com/200/300', isFav:true,},
+            ]
+     
+           
         };
      
     },
@@ -18,8 +25,26 @@ const app = Vue.createApp({
         },
         toggleShowBook () {
             this.showBook = !this.showBook
-        }
+        },
+        handleEvent(e, data){
+          if  (data) {console.log(e)}
+        },
+        handleMouseMove(e){
+this.x=e.offsetX
+this.y=e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav=!book.isFav
+        },
+      
+},
+computed:
+{
+   filteredBooks () {
+        return this.books.filter((book)=>(book.isFav))
     }
-});
+    }
+}
+);
 
 app.mount("#app");
