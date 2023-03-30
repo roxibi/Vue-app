025cf6cfd87cab39ae61ed504482313fe1402471
Roxana Bita <roxana.bita@gmail.com>
2023-03-30 16:33:10 +0200

    slots and teleport

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,16 +1,69 @@
+
+
 <template>
+<div>
+
+ <h1>{{ title }}</h1>
+
+ <div v-if="showModal">
+
+ <Modal  @close="toggleModal">
+
+ <h1>this is the normal slot</h1>
+
+<template v-slot:links>
+  <a href="#">shop here</a>
+</template>
+
+</Modal>
+</div>
+
+
+<div v-if="showModaltwo">
  
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ <Modal  @close="toggleModaltwo">
+
+  <h1>econd modal here</h1>
+
+<template v-slot:links>
+  <a href="#">shop here</a>
+</template>
+
+</Modal>
+</div>
+
+<button @click="toggleModaltwo">show Modal 2</button>
+ <button @click="toggleModal">open modal</button>
+ <input type="text" ref="name">
+ <button @click="handleClick">click</button>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal-comp.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Modal},
+ data () {
+  return{
+  title:'my first vue app', 
+
+ 
+  showModal:false,
+
+
+  showModaltwo:false,
+}
+  },
+  methods:
+// {handleClick(){this.$refs.name.classList.add("active")}},
+{
+  toggleModal(){this.showModal = !this.showModal},
+  toggleModaltwo(){this.showModaltwo=!this.showModaltwo}
+}
 }
 </script>
 
